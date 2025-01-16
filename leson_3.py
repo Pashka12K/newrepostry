939e47476f449001b29506188f2194899f509ad3
Pashka12K <Pavlikpulim@gmail.com>
2025-01-16 20:22:43 +0200

    Add files via upload

--- leson_3.py
+++ leson_3.py
@@ -1,22 +1,21 @@
 import datetime
 from datetime import datetime
-try:
-    def days_difference(date: str) -> int:
-        # Перетворюємо рядок дати у форматі 'YYYY-MM-DD' у об'єкт datetime
-        input_date = datetime.strptime(date, '%Y-%m-%d')
-    
-        # Отримуємо поточну дату
-        current_date = datetime.today()
-    
-        # Обчислюємо різницю між поточною датою та заданою
-        delta = current_date - input_date
-    
-        # Повертаємо кількість днів як ціле число
-        return delta.days
-except ValueError:
-    # Обробка помилок у випадку неправильного формату дати
-        raise ValueError("Некоректний формат дати. Використовуйте формат 'РРРР-ММ-ДД'.")
 
-
+def days_difference(date: str) -> int:
+        try:
+                # Перетворюємо рядок дати у форматі 'YYYY-MM-DD' у об'єкт datetime
+                input_date = datetime.strptime(date, '%Y-%m-%d')
+        
+                # Отримуємо поточну дату
+                current_date = datetime.today()
+        
+                # Обчислюємо різницю між поточною датою та заданою
+                delta = current_date - input_date
+        
+                # Повертаємо кількість днів як ціле число
+                return delta.days
+        except ValueError:
+        # Обробка помилок у випадку неправильного формату дати
+                raise ValueError("Некоректний формат дати. Використовуйте формат 'РРРР-ММ-ДД'.")
 
 
